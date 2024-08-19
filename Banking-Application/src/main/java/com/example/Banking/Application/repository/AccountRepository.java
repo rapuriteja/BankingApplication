5cb8a0ac67a4ee5fbba953ad215fb4cc0a0c5474
Rapuri Teja <rapuriteja@gmail.com>
2024-08-19 11:28:02 +0530

    Delete Banking-Application directory

--- Banking-Application/src/main/java/com/example/Banking/Application/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Banking.Application.repository;
-
-import com.example.Banking.Application.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
